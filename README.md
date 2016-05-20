b6ffd42d5b05c196f951acdeb56e78dcf1051bf6
Evan Jacobs <evan.jacobs@enigma.io>
2016-05-19 22:47:12 -0400

    Actually make this a real middleware, major refactor
    See the README, the usage of this module is pretty different
    compared to the original author's work.

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# Transparent Proxy Middleware
+# middleware-proxy
 
-This is an extremely simple tool that is used to make development a little faster when working on web applications. The proxy allows you to access your static html through a node server and redirect your RESTful calls to the implementing server.
+This is a simple tool that smooths the difficulties of working on integrated projects where the backend is hosted elsewhere from where you are developing your frontend. The proxy allows you to access your static HTML through a node server and redirect your RESTful calls to the implementing server.
 
-Basic example:
+## usage
+
+```js
+const proxy = require('middleware-proxy');
+
+/**
+ * Returns a middleware function that selectively proxies incoming requests to another server.
+ *
+ * @param  {string|RegExp} matcher - used to detect if the middleware should be applied against the request URL
+ * @param  {string} server - a fully qualified server address, e.g. http://localhost:8080
+ * @param  {string} [path_to_strip] - (optional) a leading string that should be removed from the request URI
+ *
+ * @return {function} middleware, suitable for use in express or connect
+ */
+proxy(matcher, server, path_to_strip)`
+```
+
+### basic express example
 
 ```js
 const express = require('express');
@@ -15,21 +32,52 @@ app.configure(function() {
     app.use(express.static(__dirname + '/app'));
 });
 
-app.get('/service/*', function(req, res) {
-    proxy(req,res, 'localhost', 8080);
-});
+app.use(proxy('/service', 'http://localhost:8080'));
 
-app.post('/service/*', function(req, res) {
-    proxy(req,res, 'localhost', 8080);
-});
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log(`Express server listening on port ${app.get('port')}`);
 });
 ```
 
-This will send all get and post requests with paths starting with `/service` to `http://localhost:8080`.
+This will send all requests starting with `/service` to `http://localhost:8080`.
 
-<br />
-<br />
+### using [budo](https://www.npmjs.com/package/budo) (a browserify livereload solution)
+
+```js
+const proxy = require('middleware-proxy');
+
+require('budo')('index.js', {
+    browserify: {
+        debug: true,
+    },
+
+    host: '0.0.0.0',
+    live: true,
+    open: true,
+    portfind: true,
+    pushstate: true,
+    serve: 'assets/bundle.js',
+    stream: process.stdout,
+
+    middleware: [proxy('/service', 'http://localhost:8080')],
+});
+
+```
+
+## removing an API prefix
+
+Let's say your integration environment has chosen `/api` as the mount point for communicating with your application's backend APIs. However, you're building your frontend in a separate project and can't easily run a dev server that joins both functionalities. `middleware-proxy` solves this issue by allowing a fifth optional parameter that defines the part of the request URL to be stripped away when proxying the request to your destination host.
+
+Assuming your remote development API host is at `http://localhost:8080` and has a RESTful collection called `/posts`, you would write your AJAX calls to `GET` from `/api/posts` and use the following configuration:
+
+```js
+proxy('/api/posts', 'http://localhost:8080', '/api');
+```
+
+The request is transparently redirected behind the scenes:
+
+```
+GET /api/posts -> GET http://localhost:8080/posts
+```
+
 <sub>Based on the work in [rgaskill/dev-rest-proxy](https://github.com/rgaskill/dev-rest-proxy)</sub>
--- index.js
+++ index.js
@@ -1,34 +1,100 @@
-const http = require('http');
+const request = require('request');
+const url = require('url');
 
-module.exports = function(inbound_request, inbound_response, server, port, path_to_strip) {
-    const options = {
-        hostname: server,
-        port: port,
-        method: inbound_request.method,
-        path: inbound_request.url,
-        headers: {},
-    };
-
-    if (path_to_strip && options.path.substring(0, path_to_strip.length) === path_to_strip) {
-        options.path = options.path.substring(path_to_strip.length)
+/**
+ * Returns a middleware function that selectively proxies incoming requests to another server.
+ *
+ * @param  {string|RegExp} matcher - used to detect if the middleware should be applied against the request URL
+ * @param  {string} server - a fully qualified server address, e.g. http://localhost:8080
+ * @param  {string} [path_to_strip] - (optional) a leading string that should be removed from the request URI
+ *
+ * @return {function} middleware, suitable for use in express or connect
+ */
+module.exports = function(matcher, server, path_to_strip) {
+    if (typeof matcher !== 'string' && !(matcher instanceof RegExp)) {
+        throw new Error('matcher (first arg) must be a string or regex');
+    } else if (typeof server !== 'string') {
+        throw new Error('server (second arg) must be a string');
+    } else if (path_to_strip && typeof path_to_strip !== 'string') {
+        throw new Error('path_to_strip (optional third arg) must be a string');
     }
 
-    options.headers = inbound_request.headers;
-    options.headers.host = server + ':' + port ;
+    let {
+        hostname,
+        port,
+        protocol,
+    } = url.parse(server);
 
-    const outbound_request = http.request(options, (outbound_response) => {
-        const headers = outbound_response.headers;
+    hostname = hostname || 'localhost';
+    port = port ? parseInt(port, 10) : 80;
+    protocol = protocol || 'http://';
 
-        if (headers['set-cookie']) {
-            headers['set-cookie'] = headers['set-cookie'].map(cookie => cookie.replace(/; ?domain=[^;]*/ig, ''));
+    /**
+     * generated middleware
+     *
+     * @param {object} internal_request - a Node ClientRequest object (https://nodejs.org/api/http.html#http_class_http_clientrequest)
+     * @param {object} internal_response - a Node ServerResponse object
+     *                                    (https://nodejs.org/api/http.html#http_class_http_serverresponse)
+     * @param {function} next - function to skip to the next middleware
+     *
+     * @return {void}
+     */
+    return function (internal_request, internal_response, next) {
+        if (typeof matcher === 'string' && internal_request.url.indexOf(matcher) !== 0) {
+            return next();
+        } else if (matcher instanceof RegExp && !internal_request.url.match(matcher)) {
+            return next();
         }
 
-        inbound_response.writeHead(outbound_response.statusCode, headers);
+        const options = {
+            headers: {
+                host: `${hostname}:${port}`,
+            },
+            method: internal_request.method,
+        };
 
-        outbound_response.on('data', chunk => inbound_response.write(chunk));
-        outbound_response.on('end', () => inbound_response.end());
-    });
+        // non-destructively copy over the original headers
 
-    inbound_request.on('data', chunk => outbound_request.write(chunk));
-    inbound_request.on('end', () => outbound_request.end());
+        for (let key in internal_request.headers) {
+            if (internal_request.headers.hasOwnProperty(key)) {
+                options.headers[key] = options.headers[key] || internal_request.headers[key];
+            }
+        }
+
+        let path = internal_request.url;
+
+        // if given, remove a prefix from the proxied request URL
+
+        if (path_to_strip && path.substring(0, path_to_strip.length) === path_to_strip) {
+            path = path.substring(path_to_strip.length);
+        }
+
+        options.jar = true; // enables passing of cookies
+        options.url = `${protocol}//${hostname}:${port}${path}`;
+
+        const external_request = request(options);
+
+        external_request.on('error', error => console.error(error));
+
+        external_request.on('response', external_response => {
+            const headers = external_response.headers;
+
+            // incoming cookies will likely have the domain of the remote server, so the domain portion
+            // must be stripped so they will work as expected
+
+            if (headers['set-cookie']) {
+                headers['set-cookie'] = headers['set-cookie'].map(cookie => cookie.replace(/; ?domain=[^;]*/ig, ''));
+            }
+
+            // pipe the response from the proxied request into the original response object the client will receive
+
+            internal_response.writeHead(external_response.statusCode, headers);
+
+            external_response.on('data', chunk => internal_response.write(chunk));
+            external_response.on('end', () => internal_response.end());
+        });
+
+        // pipe the data from the incoming request to the outgoing proxied request
+        internal_request.pipe(external_request);
+    }
 };
